51e5e80baead818f7fe81b72f14e232a35be708b
huskyhoochu <dfg1499@gmail.com>
2018-01-14 08:11:22 +0900

    lecture update

--- main.js
+++ main.js
@@ -75,4 +75,134 @@
 // }
 
 // Lecture: boolean logic and switch
+//
+// var age = 16;
+//
+// if (age < 20) {
+//     console.log('John is a teenager');
+// } else {
+//     console.log('John is a man.');
+// }
+//
+// var job = 'teacher';
+//
+// job = prompt('What does John do?');
+//
+// switch (job) {
+//     case 'teacher':
+//         console.log('John teaches kids');
+//         break;
+//     case 'driver':
+//         console.log('John drives a car');
+// }
 
+// Lecture: Functions
+//
+// function calculateAge(yearOfBirth) {
+//     var age = 2016 - yearOfBirth;
+//     return age;
+// }
+//
+// var ageJohn = calculateAge(1990);
+// var ageMike = calculateAge(1969);
+// console.log(ageJohn);
+// console.log(ageMike);
+//
+// function yearsUntilRetirement(name, yearOfBirth) {
+//     var age = calculateAge(yearOfBirth);
+//     var retirement = 65 - age;
+//     console.log(name + ' retires in ' + retirement + ' years.');
+// }
+//
+// yearsUntilRetirement('John', 1990);
+
+// Lecture: Statements and Expressions
+//
+// function someFun(par) {
+//
+// }
+//
+// var someFun = function (par) {
+//
+// }
+// Lecture: Arrays
+//
+// var names = ['John', 'Jane', 'Mark'];
+// var years = new Array(1990, 1969, 1948);
+//
+// console.log(names[2]);
+// names[1] = 'Ben';
+// console.log(names);
+// console.log(years);
+//
+// var John = ['John', 'Smith', 1990, 'teacher', false];
+// John.push('blue');
+// John.unshift('Mr.');
+// John.pop();
+//
+// console.log(John);
+//
+// alert(John.indexOf('Smith'));
+//
+
+// Lecture: Objects
+//
+// var john = {
+//     name: 'John',
+//     lastName: 'Smith',
+//     yearOfBirth: 1990,
+//     job: 'teacher',
+//     isMarried: false
+// };
+//
+// console.log(john.lastName);
+//
+// var xyz = 'job';
+// console.log(john[xyz]);
+//
+// john.lastName = 'Miller';
+//
+// console.log(john);
+//
+// var jane = new Object();
+// jane.name = 'Jane';
+// jane.lastName = 'Smith';
+
+// Lecture: Objects and Methods
+//
+// var john = {
+//     name: 'John',
+//     lastName: 'Smith',
+//     yearOfBirth: 1990,
+//     job: 'teacher',
+//     isMarried: false,
+//     family: ['Jane', 'Mark', 'Bob'],
+//     calculateAge: function (yearOfBirth) {
+//         return 2016 - yearOfBirth;
+//     }
+// };
+//
+// console.log(john);
+// console.log(john.calculateAge(1990));
+
+// Lecture: Loops
+//
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+//
+// for (var v = 0; v < names.length; v++) {
+//     console.log(names[v]);
+// }
+//
+// for (var f = names.length - 1; f >= 0; f--) {
+//     console.log(names[f]);
+// }
+
+var g = 0;
+while (g < names.length) {
+    console.log(names[g]);
+    g++;
+}
