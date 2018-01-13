178b1c931704b493714285bb6edb67d6658aacdb
huskyhoochu <dfg1499@gmail.com>
2018-01-13 20:28:10 +0900

    practice updated

--- main.js
+++ main.js
@@ -33,9 +33,46 @@
 // alert(you + ' ' + are + ' ' + jacob + ' ' +isMarried);
 //
 
-// lecture: operators
-var birthYear = 2016 - 26;
+// // lecture: operators
+// var now = 2016;
+// var birthYear = 2016 - 26;
+//
+// birthYear = now - 26 * 2;
+// // 2016 - 52 = 1964
+//
+// console.log(birthYear);
+//
+// var ageJohn = 30;
+// var ageMark = 30;
+// console.log(ageJohn);
+// ageJohn = (3 + 5) * 4 - 6;
+// console.log(ageJohn);
+// ageJohn++;
+// console.log(ageJohn);
 
-birthYear = 2016 -26 * 2;
+// Lecture: if/else statements
+//
+// var name = 'John';
+// var age = 26;
+// var isMarried = 'no';
+//
+// if (isMarried === 'yes') {
+//     console.log(name + ' is married!');
+// } else {
+//     console.log(name + ' will hopefully marry soon.');
+// }
+//
+// if (23 == '23') {
+//     console.log('loose equality: true');
+// } else {
+//     console.log('loose equality: false');
+// }
+//
+// if (23 === '23') {
+//     console.log('strict equality: true');
+// } else {
+//     console.log('strict equality: false');
+// }
 
-console.log(birthYear);
+// Lecture: boolean logic and switch
+
