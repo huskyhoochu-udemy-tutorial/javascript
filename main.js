ba6dffa56e05ca84228deea1e019a6daf6ee937c
huskyhoochu <dfg1499@gmail.com>
2018-01-13 20:05:31 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// console.log('Hello Husky!');
+// // Lecture: variables
+// var name = 'Husky';
+// console.log(name);
+//
+// var lastName = 'Smith';
+// console.log(lastName);
+//
+// var age = 26;
+// console.log(age);
+//
+// var fullAge = true;
+// console.log(fullAge);
+//
+// var you = 'john';
+// var are = 26;
+//
+// console.log(you + are);
+// console.log(are + are);
+//
+// var jacob, isMarried;
+//
+// // console.log(jacob);
+//
+// jacob = 'teacher';
+// isMarried = false;
+//
+// console.log(you + ' ' + are + ' ' + jacob + ' ' +isMarried);
+//
+// var myLastName = prompt('what is thr last name?');
+// console.log(myLastName);
+//
+// alert(you + ' ' + are + ' ' + jacob + ' ' +isMarried);
+//
+
+// lecture: operators
+var birthYear = 2016 - 26;
+
+birthYear = 2016 -26 * 2;
+
+console.log(birthYear);
